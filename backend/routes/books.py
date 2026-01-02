3b2feda0bc1f2abac6b9eae28852db9928b1a643
phoenixofdoomgryffindor-17 <phoenixofdoom.gryffindor@gmail.com>
2026-01-02 17:01:12 +0530

    Delete backend/routes directory

--- backend/routes/books.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-@router.get("/books")
-def books():
-    return FileResponse("frontend/pages/books.html")
--- backend/routes/dashboard.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter()
-
-@router.get("/dashboard")
-def dashboard():
-    return FileResponse("frontend/pages/dashboard.html")
