3f276be2728ae4ceb5a75aec122388a2ee05ea33
phoenixofdoomgryffindor-17 <phoenixofdoom.gryffindor@gmail.com>
2026-01-02 17:21:03 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,7 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from backend.routes import login, register, books, dashboard
+import uvicorn
+from app.app import create_app
 
-app = FastAPI()
+app = create_app()
 
-app.include_router(login.router)
-app.include_router(register.router)
-app.include_router(books.router)
-app.include_router(dashboard.router)
-
-app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
